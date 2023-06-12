cfac0b0a349be2a38a9706e75707471e76706867
Nikita A <66848627+nikita-1801@users.noreply.github.com>
2023-06-13 00:23:01 +0530

    Update header.php

--- header.php
+++ header.php
@@ -7,9 +7,8 @@ $first_part = $components[1];
 <ul class="nav">
             <li class="<?php if ($first_part=="") {echo "active"; } else  {echo "noactive";}?>"><a href="index.php">Home</a></li>   <!-- 
             <li><a href="registration.php">Donor Registration</a></li>             -->
-            <li class="<?php if ($first_part=="requests.php") {echo "active"; } else  {echo "noactive";}?>"><a href="requests.php">send Request</a></li><!-- 
-            <li><a href="viewrequest.php">View Request</a></li> -->
-            <li class="<?php if ($first_part=="camps.php") {echo "active"; } else  {echo "noactive";}?>"><a href="camps.php">Camps</a></li>
+            <li class="<?php if ($first_part=="requests.php") {echo "active"; } else  {echo "noactive";}?>"><a href="requests.php">send Request</a></li> 
+            <li class="<?php if ($first_part=="") {echo "active"; } else  {echo "noactive";}?>"><a href="viewrequest.php">View Request</a></li> 
           <!--  <li><a href="login.php">log In</a></li>-->
             <li class="<?php if ($first_part=="contact.php") {echo "active"; } else  {echo "noactive";}?>"><a href="search.php">Search</a></li> 
             <li class="<?php if ($first_part=="contact.php") {echo "active"; } else  {echo "noactive";}?>"><a href="contact.php">Contact Us</a></li>
